dc9d3970f93e401190de35bba28c5ebcd68bcfaf
Sai-Bhuvan <107638614+Sai-Bhuvan@users.noreply.github.com>
2023-01-08 18:32:00 +0530

    Create faculty.java

---
+++ q10/faculty.java
@@ -0,0 +1,8 @@
+package q10;
+
+
+    public class faculty {
+        public String member_name,designation,joining_date;
+        public int age,experiance,no_of_designations,projects;
+        public  String subjects;
+    }
