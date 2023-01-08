bbe7fc8bb5bc91c823507790d01b7df397f437c0
Sai-Bhuvan <107638614+Sai-Bhuvan@users.noreply.github.com>
2023-01-08 18:31:25 +0530

    Create department.java

---
+++ q10/department.java
@@ -0,0 +1,12 @@
+package q10;
+import q10.faculty;
+
+public interface department {
+
+        void readdata(faculty f);
+        void putdata(faculty f);
+        void numberdesignation(faculty f);
+        void projects(faculty f);
+
+
+}
